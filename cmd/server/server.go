9614a9216917952d4926ed83bb6b47811922f79e
Pavel <seleznev.pavlex@gmail.com>
2022-07-31 15:53:37 +0300

    add grpc rest gateway

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
 	apiPkg "gitlab.ozon.dev/ArtoriasAbW/homework-01/internal/api"
 	bookPkg "gitlab.ozon.dev/ArtoriasAbW/homework-01/internal/pkg/core/book"
 	pb "gitlab.ozon.dev/ArtoriasAbW/homework-01/pkg/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func runGrpc() {
+func runGRPC() {
 	listener, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatal(err.Error())
@@ -26,6 +31,36 @@ func runGrpc() {
 	}
 }
 
+func runREST() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux(
+		runtime.WithIncomingHeaderMatcher(headerMatcherREST),
+	)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	if err := pb.RegisterBookHandlerFromEndpoint(ctx, mux, ":8081", opts); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
+
+func headerMatcherREST(key string) (string, bool) {
+	switch key {
+	case "Custom":
+		return key, true
+	default:
+		return key, false
+	}
+}
+
 func main() {
-	runGrpc()
+	
+	go runREST()
+	runGRPC()
 }
