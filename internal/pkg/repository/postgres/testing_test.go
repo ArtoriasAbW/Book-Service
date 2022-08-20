1f4ba5690e22ca30f55473f0d735ddccb79a64ee
Pavel <seleznev.pavlex@gmail.com>
2022-08-20 14:43:03 +0300

    [dev/GOHW-5] repo unit test initial

--- internal/pkg/repository/postgres/testing_test.go
+++ internal/pkg/repository/postgres/testing_test.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"testing"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jmoiron/sqlx"
 	repoPkg "gitlab.ozon.dev/ArtoriasAbW/homework-01/internal/pkg/repository"
@@ -12,24 +14,18 @@ type bookRepoFixtures struct {
 	dbMock   sqlmock.Sqlmock
 }
 
-// func setUp(t *testing.T) bookRepoFixtures {
-// 	var fixture bookRepoFixtures
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("some error")unc setUp(t *testing.T) bookRepoFixtures {
-// 	var fixture bookRepoFixtures
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("some error")
-// 	}
-// 	fixture.db = sqlx.NewDb(db, "sqlmock")
-// 	fixture.dbMock = mock
-// 	fixture.bookRepo = *NewRepository(fixture.db)
-// 	return fixture
-// }
-// 	}
-// 	fixture.db = sqlx.NewDb(db, "sqlmock")
-// 	fixture.dbMock = mock
-// 	fixture.bookRepo = *NewRepository(fixture.db)
-// 	return fixture
-// }
+func setUp(t *testing.T) bookRepoFixtures {
+	var fixture bookRepoFixtures
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("some error")
+	}
+	fixture.db = sqlx.NewDb(db, "sqlmock")
+	fixture.dbMock = mock
+	fixture.bookRepo = NewRepository(fixture.db)
+	return fixture
+}
+
+func (f *bookRepoFixtures) tearDown() {
+	f.db.Close()
+}
--- internal/pkg/repository/postgres/user_test.go
+++ internal/pkg/repository/postgres/user_test.go
@@ -1,1 +1,52 @@
 package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.ozon.dev/ArtoriasAbW/homework-01/internal/pkg/repository/models"
+)
+
+func TestNewUsers(t *testing.T) {
+	// arrange
+	t.Run("success", func(t *testing.T) {
+		f := setUp(t)
+		defer f.tearDown()
+
+		queryStore := regexp.QuoteMeta(`INSERT INTO users (username) VALUES ($1) RETURNING id`)
+		rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+		f.dbMock.ExpectQuery(queryStore).WithArgs("username").WillReturnRows(rows)
+
+		// act
+		id, err := f.bookRepo.AddUser(context.Background(), models.User{
+			Username: "username",
+		})
+
+		// assert
+		require.NoError(t, err)
+		assert.Equal(t, uint64(1), id)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		f := setUp(t)
+		defer f.tearDown()
+
+		queryStore := regexp.QuoteMeta(`INSERT INTO users (username) VALUES ($1) RETURNING id`)
+		f.dbMock.ExpectQuery(queryStore).WithArgs("username").WillReturnError(errors.New("some error"))
+
+		// act
+		_, err := f.bookRepo.AddUser(context.Background(), models.User{
+			Username: "username",
+		})
+
+		// assert
+		assert.Equal(t, err, fmt.Errorf("Repository.AddUser: to sql: %w", errors.New("some error")))
+
+	})
+}
