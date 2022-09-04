c53a0cb3c6cc40f20d1cb989850ad0ae825ff44f
Pavel <seleznev.pavlex@gmail.com>
2022-09-04 16:39:31 +0300

    fix pipeline

--- migrate-test.sh
+++ migrate-test.sh
@@ -4,7 +4,7 @@ export MIGRATION_DIR="./migrations"
 export DB_DSN="host=localhost port=5433 user=test_user password=test_password dbname=book_service sslmode=disable"
 
 if [ "$1" = "--dryrun" ]; then
-    $(LOCAL_BIN)/goose -v -dir ${MIGRATION_DIR} postgres "${DB_DSN}" status
+    bin/goose -v -dir ${MIGRATION_DIR} postgres "${DB_DSN}" status
 else
-    $(LOCAL_BIN)/goose -v -dir ${MIGRATION_DIR} postgres "${DB_DSN}" up
+    bin/goose -v -dir ${MIGRATION_DIR} postgres "${DB_DSN}" up
 fi
