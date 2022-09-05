497d344d9e2ffd84df9796d1e229648b76c7ad1f
Pavel <seleznev.pavlex@gmail.com>
2022-09-05 10:30:33 +0300

    fix migrate

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,30 +1,13 @@
 package logger
 
 import (
-	"encoding/json"
-
 	"go.uber.org/zap"
 )
 
 var Logger *zap.Logger
 
 func InitLogger() error {
-	rawJSON := []byte(`{
-		"level": "debug",
-		"encoding": "json",
-		"outputPaths": ["stdout"],
-		"errorOutputPaths": ["stderr"],
-		"encoderConfig": {
-		  "messageKey": "message",
-		  "levelKey": "level",
-		  "levelEncoder": "lowercase"
-		}
-	  }`)
-	var cfg zap.Config
-	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-		return err
-	}
-	zapLogger, err := cfg.Build()
+	zapLogger, err := zap.NewDevelopment()
 	if err != nil {
 		return err
 	}
--- migrate-test.sh
+++ migrate-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export MIGRATION_DIR="./migrations"
-export DB_DSN="host=localhost port=5432 user=test_user password=test_password dbname=book_service sslmode=disable"
+export DB_DSN="host=postgres port=5432 user=test_user password=test_password dbname=book_service sslmode=disable"
 
 if [ "$1" = "--dryrun" ]; then
     bin/goose -v -dir ${MIGRATION_DIR} postgres "${DB_DSN}" status
